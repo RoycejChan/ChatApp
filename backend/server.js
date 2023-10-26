94e2ae1b95fd8e380e2b63ab0447edce825b6cba
Roycej-Chan <Rouyce23@gmail.com>
2023-10-25 18:04:56 -0700

    //CHANGE LEEAVE CHROOM TO LEAVE CHAT ROOM , also added some css to login page  //FIXME:ADDED user log when user leaves room it will show on everyone screen, like a feed, currently only shows on that persons feed not other, nvm it shows undefined has left room instead of username too     //SHOW on dom for 5 seconds when user enters or leaves currentRoom //FIXME: ERROR <p> message if user doesnt enter login input
    // TODO:
    //UPDATE MESSAGES TO CURENT ROOM WHEN SWITCHING ROOMS
    //DELETE messages only if they were the sender
    //INCLUDE POCKET BASE FOR USER NAME AND MESSAGES ?
    //ADD CSS

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 app.use(cors({
-    origin: "http://localhost:5174", // Allow requests from the frontend
+    origin: "http://localhost:5173", // Allow requests from the frontend
     methods: ["GET", "POST"]
 }));
 
@@ -13,11 +13,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5174", // Set the same origin for Socket.IO
+        origin: "http://localhost:5173", // Set the same origin for Socket.IO
         methods: ["GET", "POST"]
     }
 });
-
+//10/25//2023
 //DONE TODOS
 // -- HIDE MESSAGEINPUT MAKE USER NOT BE ABLE TO TYPE A MESSAGE UNTIL A ROOM IS ENTERED. 
 // --WHEN USER ENTERES ANOTHER ROOM, LEAVE BUTTON WILL BE TIED TO CURRENT ROOM AND CLICKING IT WILL LEAVE CURRENT ROOM WITHOUT HAVINGTO INPUT ROOM NUMBER
@@ -26,16 +26,21 @@ const io = new Server(server, {
 // DELETE CURRENT ROOM MESSAGES WHEN ENTERING NEW ONE
 //IF USER TRIES TO ENTER ROOM THEY ARE ALREADY IN, return
 //DELETE html inputs everytime user switches, leaves, or join room
-
 //INCLUDE USERNAME FOR USER TO JOIN CHAT
 //TIE USERNAME TO MESSAGE SENT
+//added useeffect to react on even listeners bc socket.io declared multiple socket connections and display li tags multiple times.
+
+//CHANGE LEEAVE CHROOM TO LEAVE CHAT ROOM
+//FIXME:ADDED user log when user leaves room it will show on everyone screen, like a feed, currently only shows on that persons feed not other, nvm it shows undefined has left room instead of username too
+    //SHOW on dom for 5 seconds when user enters or leaves currentRoom
+//FIXME: ERROR <p> message if user doesnt enter login input
 
 
 // TODO:
 //UPDATE MESSAGES TO CURENT ROOM WHEN SWITCHING ROOMS
-//SHOW on dom for 5 seconds when user enters or leaves currentRoom
 //DELETE messages only if they were the sender
 //INCLUDE POCKET BASE FOR USER NAME AND MESSAGES ?
+//ADD CSS
 
 
 io.on("connection", (socket) => {
@@ -62,10 +67,9 @@ io.on("connection", (socket) => {
     });
 
 
-    socket.on("leaveroom", () => {
+    socket.on("exitRoom", ({username}) => {
         socket.leave(currentRoom);
-        socket.emit("leaveChat");
-        console.log(`${socket.id} left ${currentRoom}`)
+        socket.emit("leaveChat", username);
         currentRoom = null;
     });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [room, setRoom] = useState(0);
   const [userActive, setUser] = useState(false);
   const [username, setUsername] = useState("");
-
+  const [userlog, setuserLog] =useState("");
   useEffect(() => {
     // Initialize the socket connection
     socket = io(ENDPOINT);
@@ -33,13 +33,6 @@ function App() {
       }
     });
 
-    socket.on('leaveChat', () => {
-      console.log("You have left the chat room app");
-      const msglist = document.getElementById("msglist");
-      setMessage("");
-      setRoom("");
-      setUser(false);
-    });
 
     socket.on('changeRoom', () => {
       console.log("Changed UI to current room");
@@ -48,7 +41,14 @@ function App() {
       setMessage("");
       setRoom("");
     });
-
+    socket.on('leaveChat', ({username}) => {
+      console.log("You have left the chat room app");
+      let log = `${username} has left the room`;
+      setuserLog(log);
+      setMessage("");
+      setRoom("");
+      setUser(false);
+    });
     //END FUNCTIONS
 
     // Clean up by removing event listeners when the component unmounts
@@ -67,6 +67,8 @@ function App() {
   const joinroom = (room) => {
     if (room === 0) {
       console.log("Please enter a room");
+      const errmsg = document.getElementById("login-error")
+      errmsg.innerHTML = "Please enter a username or room number";
       return;
     } else {
       socket.emit("joinroom", room);
@@ -74,55 +76,80 @@ function App() {
     }
   };
 
-  const leaveroom = () => {
-    socket.emit("leaveroom", room);
+  const exitRoom = () => {
+    socket.emit("exitRoom", ( {room, username}));
   };
+  
 
 
 
   return (
     <>
-          {/* DISPLAY APP ONLY WHEN USER ENTERS ROOM */}
+
+
+
+
+
+     {/* DISPLAY APP ONLY WHEN USER ENTERS ROOM */}
     {userActive ? 
-    <div>
-      <h1>WELCOME TO THE CHAT ROOM</h1>
-      <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className='userInputs'
-        />
+    <div className='chatroom-container'>
+          <h1>WELCOME TO THE CHAT ROOM</h1>
+          <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className='userInputs'
+          />
 
-        <button onClick={sendMessage}>Send Message</button>
+          <button onClick={sendMessage}>Send Message</button>
         
+          <input type="string"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          />
 
-        <ul id="msglist">
+          <input type="number"
+          onChange={(e) => setRoom(e.target.value)}
+          className='userInputs'
+          value={room}
+          />
+          <button onClick={()=>joinroom(room)}>join room</button>
+          <button onClick={()=>exitRoom()} >EXIT CHAT ROOM</button>
+          <ul id="msglist">
 
-        </ul>
-      </div> : <h1>JOIN A ROOM TO START CHATTING</h1>
-    }
-          {/* END CONDITIONAL RENDER */}
+          </ul>
+          <p className='userLogs'>{userlog}</p>
+      </div> 
 
-
-    {/* USER INPUTS FOR NAVIGATING THROUGH  */}
-
-      <input type="string"
-      onChange={(e) => setUsername(e.target.value)}
-      value={username}
-      />
-
-      <input type="number"
-      onChange={(e) => setRoom(e.target.value)}
-      className='userInputs'
-      value={room}
-      />
-      <button onClick={()=>joinroom(room)}>join room</button>
-      <button onClick={()=>leaveroom()}>leave room</button>
-    {/* END USE NAVIGATION */}
-
-      {/* CHAT MESSAGES  */}
-      <p className='userLogs'></p>
+      : 
+      //DISPLAY WHEN USER ENTERS SITE 
       
+          <div className='start-container mt-32 flex flex-col items-center'>
+
+            <h1 className='h-28 bg-blue-700 text-center flex items-center justify-center text-2xl w-full'>Chat.io 💬</h1>
+
+              <div className="joinInputs flex flex-col w-full items-center justify-center gap-8 h-full bg-purple-700">
+
+              <label>Username</label>
+              <input type="string"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              className='userInputs w-5/6 p-4'
+              placeholder='Enter your username ...'
+              />
+
+              <label>Room</label>
+              <input type="number"
+              onChange={(e) => setRoom(e.target.value)}
+              className='userInputs w-5/6 p-4'
+              value={room}
+              />
+              <button onClick={()=>joinroom(room)}>join room</button>
+              <p id="login-error"></p>
+              </div>
+          </div>
+    }
+
     </>
     
   );
